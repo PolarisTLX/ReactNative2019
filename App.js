afca8e97913951a01971ff37f1de396f4ebb308e
Paul Rail <paul.a.rail@gmail.com>
2019-07-30 10:13:00 -0400

    bottomTabNavigator + ways to perform additional actions on navigation, such as check authentication

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 import HomeScreen from './src/home';
 import UserScreen from './src/users';
@@ -20,54 +20,98 @@ class App extends Component {
     return (
       <View style={styles.mainView}>
         <SafeAreaView>
-
+          <Text></Text>
         </SafeAreaView>
       </View>
     );
   }
 };
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen,
-    // can also come here to apply style to just the home screen navbar:
-    // navigationOptions: ({ navigation }) => ({
-    //   title: 'Home Screen',
-    //   to apply an image logo instead of a title:
-    //   headerTitle: LogoTitle
-    // }),
-  },
-  Users: {
-    screen: UserScreen
-  }
-},{
-  initialRouteName: 'Home',
-  // to apply uniform header style to all screens:
-  defaultNavigationOptions: {
-    headerStyle: {
-      backgroundColor: '#f4511e'
+const AppNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Users:UserScreen,
+}, {
+  initialRouteName:'Home',
+  order: ['Users', 'Home'],
+  tabBarOptions:{
+    activeTintColor:'red',
+    activeBackgroundColor:'blue',
+    inactiveTintColor: 'yellow',
+    inactiveBackgroundColor: 'grey',
+    // showLabel: false  //hides the text on that nav-button
+    style: {
+      // backgroundColor: 'green'
     },
-    headerTintColor:'blue',
-    headerTitleStyle: {
-      flex: 1,
-      textAlign: 'center',
+    labelStyle: {
+      fontSize: 20,
       fontWeight: 'bold',
-      color: 'white'
-    },
-
+      paddingBottom: 10      
+    }
   },
-  // mode: 'modal',  //does not work on android
-  onTransitionStart:() => { 
-    alert("This is a Navigation 'onTransitionStart' function") 
-  }
-})
+  defaultNavigationOptions:()=>({
+    // how to capture info on where the User is going and perform actions based on that:
+    tabBarOnPress:({navigation, defaultHandler})=>{
+      alert(navigation)
 
-const styles = StyleSheet.create({
-  mainView: {
-    flex: 1,
-    paddingTop: 50,
-    backgroundColor: 'white',
-  },
+      // this can be used to overide behavior of bottomnavbar:
+      if(navigation.state.key === 'Users'){
+        // if they clicked on 'Users', allow them to go to 'Users', 
+        // or perform other things in between:
+
+        // perhaps check if they are authenticated.  
+        // if they are not, can re-direct them to the login screen.
+
+        alert('Hello')
+
+        navigation.navigate('Users')
+      } else {
+        defaultHandler();
+      }
+    }
+  })
 });
+
+// const AppNavigator = createStackNavigator({
+//   Home: {
+//     screen: HomeScreen,
+//     // can also come here to apply style to just the home screen navbar:
+//     // navigationOptions: ({ navigation }) => ({
+//     //   title: 'Home Screen',
+//     //   to apply an image logo instead of a title:
+//     //   headerTitle: LogoTitle
+//     // }),
+//   },
+//   Users: {
+//     screen: UserScreen
+//   }
+// },{
+//   initialRouteName: 'Home',
+//   // to apply uniform header style to all screens:
+//   defaultNavigationOptions: {
+//     headerStyle: {
+//       backgroundColor: '#f4511e'
+//     },
+//     headerTintColor:'blue',
+//     headerTitleStyle: {
+//       flex: 1,
+//       textAlign: 'center',
+//       fontWeight: 'bold',
+//       color: 'white'
+//     },
+
+//   },
+//   // mode: 'modal',  //does not work on android
+//   onTransitionStart:() => { 
+//     alert("This is a Navigation 'onTransitionStart' function") 
+//   }
+// })
+
+// const styles = StyleSheet.create({
+//   mainView: {
+//     flex: 1,
+//     paddingTop: 50,
+//     backgroundColor: 'white',
+//   },
+// });
 
 export default createAppContainer(AppNavigator);
